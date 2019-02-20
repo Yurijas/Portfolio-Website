f77e2bc139c8de238175398b688a4a220196841b
Yurijas <yuriebmori012@gmail.com>
2019-02-20 22:22:11 +0900

    connected database

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #         'sqlite:///' + os.path.join(BASEDIR, 'app.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:venezuel@localhost:5432/portfolio_website'
